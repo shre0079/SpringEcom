8479d0f728812c8824cd88a44c4240eb54f43ce1
Shreyash <shreyashshree0079@gmail.com>
2025-08-14 18:38:29 +0530

    service method add getProductbyid

--- src/main/java/com/shre/SpringEcom/service/ProductService.java
+++ src/main/java/com/shre/SpringEcom/service/ProductService.java
@@ -15,4 +15,8 @@ public class ProductService {
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
+
+    public Product getProductById(int id) {
+        return productRepository.findById(id).orElse(new Product(-1));
+    }
 }
