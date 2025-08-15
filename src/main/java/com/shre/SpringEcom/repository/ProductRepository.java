04dcf9a7d669ea9e61edd897f254e83370a9739f
Shreyash <shreyashshree0079@gmail.com>
2025-08-15 11:02:05 +0530

    Sql query in repo

--- src/main/java/com/shre/SpringEcom/repository/ProductRepository.java
+++ src/main/java/com/shre/SpringEcom/repository/ProductRepository.java
@@ -8,4 +8,10 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    @Query("SELECT p from Product p WHERE " +
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Product> searchProducts(String keyword);
 }
