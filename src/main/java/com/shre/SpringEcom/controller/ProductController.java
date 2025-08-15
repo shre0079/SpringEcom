59f7b5a5c0832a4a792a60b6f8966ebd37da1f0f
Shreyash <shreyashshree0079@gmail.com>
2025-08-15 11:04:59 +0530

    controller imports

--- src/main/java/com/shre/SpringEcom/controller/ProductController.java
+++ src/main/java/com/shre/SpringEcom/controller/ProductController.java
@@ -5,11 +5,9 @@ import com.shre.SpringEcom.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
