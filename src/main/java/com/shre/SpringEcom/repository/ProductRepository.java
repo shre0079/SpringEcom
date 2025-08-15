d948ae115f03557970a0e6292ae0d0fbf46a1c24
Shreyash <shreyashshree0079@gmail.com>
2025-08-15 11:00:54 +0530

    repo imports

--- src/main/java/com/shre/SpringEcom/repository/ProductRepository.java
+++ src/main/java/com/shre/SpringEcom/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.shre.SpringEcom.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
