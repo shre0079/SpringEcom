98945fd5cb872f176b0b4b491960501ed4d51e99
Shreyash <shreyashshree0079@gmail.com>
2025-08-15 11:02:26 +0530

    service imports

--- src/main/java/com/shre/SpringEcom/service/ProductService.java
+++ src/main/java/com/shre/SpringEcom/service/ProductService.java
@@ -4,6 +4,8 @@ import com.shre.SpringEcom.model.Product;
 import com.shre.SpringEcom.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 import java.util.List;
 
 @Service
